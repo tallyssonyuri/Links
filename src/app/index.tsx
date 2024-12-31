0aea075a7de5e9c16d0e5b425580bb5f7664f8a4
Tallysson Yuri <tallyssonyuri03@gmail.com>
2024-12-31 09:19:52 -0300

    feat: adicionado layout, styles e colors no projeto

--- src/app/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Text } from "react-native"
-
-export default function Index(){
-    return (
-        <Text>
-            Hello React Native!
-        </Text>
-    )
-}
---
+++ src/app/index/index.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { View, Image, TouchableOpacity } from "react-native";
+import { styles } from "./styles";
+import { MaterialIcons } from "@expo/vector-icons";
+import { colors } from "@/styles/colors";
+
+export default function Index() {
+    return (
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <Image 
+                    source={require("@/assets/logo.png")}
+                    style={styles.logo}
+                />
+                <TouchableOpacity activeOpacity={0.3}>
+                    <MaterialIcons 
+                        name="add"
+                        size={32} 
+                        color={colors.green[300]}
+                    />
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
+}
